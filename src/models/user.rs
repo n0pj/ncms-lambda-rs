a036b1f99537b85c4a945e186912d816309b79dd
n0pj <admin@n0pj.com>
2022-02-13 04:16:02 +0900

    user

---
+++ src/models/user.rs
@@ -0,0 +1,151 @@
+use crate::schema::user;
+use chrono::Utc;
+use diesel::{Insertable, Queryable};
+use juniper::GraphQLObject;
+use ncms_core::{Model, NewModel};
+use std::io::Error;
+use uuid::Uuid;
+
+///
+/// DB からデータを取得するための構造体
+///
+#[derive(Debug, Clone, Queryable)]
+pub struct User {
+    pub uuid: String,
+    pub name: Option<String>,
+    pub email: Option<String>,
+    pub display_name: Option<String>,
+    pub password: Option<String>,
+    pub created_at: String,
+    pub updated_at: String,
+}
+
+impl Model<ResUser, Error> for User {
+    ///
+    /// レスポンス用に変換
+    ///
+    fn to_res(&self) -> Result<ResUser, Error> {
+        Ok(ResUser {
+            uuid: self.uuid.clone(),
+            name: self.name.clone(),
+            email: self.email.clone(),
+            display_name: self.display_name.clone(),
+            password: self.password.clone(),
+            created_at: self.created_at.clone(),
+            updated_at: self.updated_at.clone(),
+        })
+    }
+}
+
+///
+/// DB へデータを作成するための構造体
+///
+#[derive(Debug, Clone, Insertable)]
+#[table_name = "user"]
+struct NewUser {
+    pub uuid: String,
+    pub name: Option<String>,
+    pub email: Option<String>,
+    pub display_name: Option<String>,
+    pub password: Option<String>,
+    pub created_at: String,
+    pub updated_at: String,
+}
+
+impl Default for NewUser {
+    fn default() -> Self {
+        let now = Utc::now().to_string();
+
+        Self {
+            uuid: Uuid::new_v4().to_string(),
+            name: None,
+            email: None,
+            display_name: None,
+            password: None,
+            created_at: now.clone(),
+            updated_at: now,
+        }
+    }
+}
+
+impl NewModel<User, NewUser, Error> for NewUser {
+    fn from_model(model: &User) -> Result<Self, Error> {
+        Ok(Self {
+            uuid: model.uuid.clone(),
+            name: None,
+            email: None,
+            display_name: None,
+            password: None,
+            created_at: model.created_at.clone(),
+            updated_at: model.updated_at.clone(),
+        })
+    }
+
+    fn to_model(&self) -> Result<User, Error> {
+        Ok(User {
+            uuid: self.uuid.clone(),
+            name: self.name.clone(),
+            email: self.email.clone(),
+            display_name: self.display_name.clone(),
+            password: self.password.clone(),
+            created_at: self.created_at.clone(),
+            updated_at: self.updated_at.clone(),
+        })
+    }
+
+    fn insert(&self) -> Result<User, Error> {
+        Ok(User {
+            uuid: self.uuid.clone(),
+            name: self.name.clone(),
+            email: self.email.clone(),
+            display_name: self.display_name.clone(),
+            password: self.password.clone(),
+            created_at: self.created_at.clone(),
+            updated_at: self.updated_at.clone(),
+        })
+    }
+
+    fn update(&self) -> Result<User, Error> {
+        Ok(User {
+            uuid: self.uuid.clone(),
+            name: self.name.clone(),
+            email: self.email.clone(),
+            display_name: self.display_name.clone(),
+            password: self.password.clone(),
+            created_at: self.created_at.clone(),
+            updated_at: self.updated_at.clone(),
+        })
+    }
+}
+
+///
+/// GraphQL のレスポンス用に変換
+///
+#[derive(Debug, Clone, GraphQLObject)]
+struct ResUser {
+    pub uuid: String,
+    pub name: Option<String>,
+    pub email: Option<String>,
+    pub display_name: Option<String>,
+    pub password: Option<String>,
+    pub created_at: String,
+    pub updated_at: String,
+}
+
+pub type Users = Vec<User>;
+pub type ResUsers = Vec<ResUser>;
+
+pub trait UserssMethods {
+    ///
+    /// レスポンス用に変換
+    ///
+    fn to_res(&self) -> ResUsers;
+}
+
+impl UserssMethods for Users {
+    fn to_res(&self) -> ResUsers {
+        self.into_iter()
+            .map(|user| user.to_res().unwrap())
+            .collect()
+    }
+}
