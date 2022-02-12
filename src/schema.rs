404631ce0255f2d1ab565118f8346c9591eada21
n0pj <admin@n0pj.com>
2022-02-13 02:42:21 +0900

    schema 更新

--- src/schema.rs
+++ src/schema.rs
@@ -3,15 +3,7 @@ table! {
         uuid -> Varchar,
         name -> Varchar,
         slug -> Varchar,
-        created_at -> Varchar,
-        updated_at -> Varchar,
-    }
-}
-
-table! {
-    kind (uuid) {
-        uuid -> Varchar,
-        name -> Varchar,
+        is_main -> Nullable<Tinyint>,
         created_at -> Varchar,
         updated_at -> Varchar,
     }
@@ -23,9 +15,11 @@ table! {
         title -> Nullable<Varchar>,
         password -> Nullable<Varchar>,
         content -> Longtext,
-        slug -> Varchar,
-        updated_at -> Varchar,
+        slug -> Nullable<Varchar>,
         created_at -> Varchar,
+        updated_at -> Varchar,
+        status_uuid -> Varchar,
+        user_uuid -> Varchar,
     }
 }
 
@@ -43,60 +37,10 @@ table! {
     post_comment (uuid) {
         uuid -> Varchar,
         content -> Longtext,
-        author -> Varchar,
         created_at -> Varchar,
         updated_at -> Varchar,
         post_uuid -> Varchar,
-    }
-}
-
-table! {
-    post_kind (uuid) {
-        uuid -> Varchar,
-        created_at -> Varchar,
-        updated_at -> Varchar,
-        post_uuid -> Varchar,
-        kind_uuid -> Varchar,
-    }
-}
-
-table! {
-    post_score (uuid) {
-        uuid -> Varchar,
-        count -> Bigint,
-        created_at -> Varchar,
-        updated_at -> Varchar,
-        post_score_kind_id -> Varchar,
-        post_status_uuid -> Varchar,
-    }
-}
-
-table! {
-    post_score_kind (id) {
-        id -> Varchar,
-        created_at -> Varchar,
-        updated_at -> Varchar,
-        kind_uuid -> Varchar,
-    }
-}
-
-table! {
-    post_status (uuid) {
-        uuid -> Varchar,
-        created_at -> Varchar,
-        updated_at -> Varchar,
-        status_uuid -> Varchar,
-        post_uuid -> Varchar,
-    }
-}
-
-table! {
-    post_user (uuid) {
-        uuid -> Varchar,
-        created_at -> Varchar,
-        updated_at -> Varchar,
         user_uuid -> Varchar,
-        post_uuid -> Varchar,
     }
 }
 
@@ -112,38 +56,27 @@ table! {
 table! {
     user (uuid) {
         uuid -> Varchar,
-        name -> Varchar,
-        email -> Varchar,
-        display_name -> Varchar,
+        name -> Nullable<Varchar>,
+        email -> Nullable<Varchar>,
+        display_name -> Nullable<Varchar>,
+        password -> Nullable<Varchar>,
         created_at -> Varchar,
         updated_at -> Varchar,
     }
 }
 
+joinable!(post -> status (status_uuid));
+joinable!(post -> user (user_uuid));
 joinable!(post_category -> category (category_uuid));
 joinable!(post_category -> post (post_uuid));
 joinable!(post_comment -> post (post_uuid));
-joinable!(post_kind -> kind (kind_uuid));
-joinable!(post_kind -> post (post_uuid));
-joinable!(post_score -> post_score_kind (post_score_kind_id));
-joinable!(post_score -> post_status (post_status_uuid));
-joinable!(post_score_kind -> kind (kind_uuid));
-joinable!(post_status -> post (post_uuid));
-joinable!(post_status -> status (status_uuid));
-joinable!(post_user -> post (post_uuid));
-joinable!(post_user -> user (user_uuid));
+joinable!(post_comment -> user (user_uuid));
 
 allow_tables_to_appear_in_same_query!(
     category,
-    kind,
     post,
     post_category,
     post_comment,
-    post_kind,
-    post_score,
-    post_score_kind,
-    post_status,
-    post_user,
     status,
     user,
 );
